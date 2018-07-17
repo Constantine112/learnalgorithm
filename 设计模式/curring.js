d95cc04634557cef91cb0ae690529a50ba06e914
Woojazz <768131566@qq.com>
2018-07-17 21:28:46 +0800

    curring

---
+++ 设计模式/curring.js
@@ -0,0 +1,46 @@
+var curring = function ( fn ) {
+    var args = []
+    return function () {
+        if ( arguments.length === 0 ) {
+            return fn.apply( this, args )
+        } else {
+            [].push.apply( args, arguments )
+            return arguments.callee
+        }
+    }
+}
+
+var cost = (function () {
+    var money = 0
+
+    return function () {
+        for (var i = 0; i < arguments.length; i++) {
+            money += arguments[i]
+        }
+        return money
+    }
+})()
+
+var cost = curring( cost )
+
+cost( 100 )
+cost( 200 )
+cost( 300 )
+console.log( cost() )
+// ClampDoor = function (name, height, weight, age) {
+//     this.name = name
+//     this.height = height
+//     this.weight = weight
+//     this.age = age
+//     return function () {
+//         return {
+//             getName: function () {
+//                 return this.name
+//             }
+//         }
+//     }
+// }
+
+function  () {
+    
+}
